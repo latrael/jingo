bb59e5e04dbc8cea5a9133a260041be94e32435f
Nabil Djaber <40780645+Quimea@users.noreply.github.com>
2019-10-22 15:20:37 -0600

    Add files via upload

--- main.sh
+++ main.sh
@@ -4,19 +4,27 @@ unalias -a #Get rid of aliases
 echo "unalias -a" >> ~/.bashrc
 echo "unalias -a" >> /root/.bashrc
 PWDthi=$(pwd)
-
+if [ ! -d $PWDthi/referenceFiles ]; then
+	echo "Please Cd into this script's directory"
+	exit
+fi
 if [ "$EUID" -ne 0 ] ;
 	then echo "Run as Root"
 	exit
 fi
 
 # List of all the functions that are going to be run:
-#RootPasswdChange
+# syncookie
+# disableipv6
+# disableguest
+# RootPasswdChange
 
 startFunctions() {
 	clear
 
 	syncookie
+	disableipv6
+	disableguest
 	RootPasswdChange
 
 	printf "\033[1;31mDone!\033[0m\n"
@@ -38,6 +46,19 @@ syncookie(){
 	cont
 }
 
+disableipv6(){
+	sudo echo "net.ipv6.conf.all.disable_ipv6 = 1" >> /etc/sysctl.conf
+	printf "\n Successfully disabled ipv6! \n"
+	cont
+
+}
+
+disableguest(){
+	printf "\n Disabling Guest Account! \n"
+	cat $PWDthi/referenceFiles/lightdm.conf > /etc/lightdm/lightdm.conf
+	cont
+}
+
 RootPasswdChange() {
 printf "Changing root's passwd \n \n"
 # changes root's password
@@ -45,6 +66,20 @@ passwd
 cont
 }
 
+# cronroot(){
+#	printf "\n Only allowing cron with root access \n"
+#	
+	#--------- Allow Only Root Cron ----------------
+	#reset crontab
+#	crontab -r
+#	cd /etc/
+#	/bin/rm -f cron.deny at.deny
+#	echo root >cron.allow
+#	echo root >at.allow
+#	/bin/chown root:root cron.allow at.allow
+#	/bin/chmod 644 cron.allow at.allow
+#	cont
+#}
 
 
 startFunctions
