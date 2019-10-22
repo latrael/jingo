db817ded18af37767880643e5548b03e020d5ab4
Nabil Djaber <40780645+Quimea@users.noreply.github.com>
2019-10-22 15:07:20 -0600

    Add files via upload

--- main.sh
+++ main.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
-#MIT Fake License
+
 unalias -a #Get rid of aliases
 echo "unalias -a" >> ~/.bashrc
 echo "unalias -a" >> /root/.bashrc
 PWDthi=$(pwd)
-if [ ! -d $PWDthi/referenceFiles ]; then
-	echo "Please Cd into this script's directory"
-	exit
-fi
+
 if [ "$EUID" -ne 0 ] ;
 	then echo "Run as Root"
 	exit
@@ -20,10 +17,12 @@ startFunctions() {
 	clear
 
 	RootPasswdChange
+	syncookie
+
 	printf "\033[1;31mDone!\033[0m\n"
 }
 
-cont() {
+cont(){
 	printf "\033[1;31mI have finished this task. Continue to next Task? (Y/N)\033[0m\n"
 	read contyn
 	if [ "$contyn" = "N" ] || [ "$contyn" = "n" ]; then
@@ -32,12 +31,19 @@ cont() {
 	fi
 	clear
 }
-RootPasswdChange() {
-printf "Changing root's passwd"
-# disables root's password
-passwd
 
+syncookie(){
+	sysctl -n net.ipv4.tcp_syncookies
+	cont
+}
+
+RootPasswdChange() {
+printf "Changing root's passwd \n \n"
+# changes root's password
+passwd
 cont
 }
+
+
 
 startFunctions
